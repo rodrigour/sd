4bcfc0f7af0184a0488559608be2037fcb79560a
Rodrigo Ruiz Coronado <rodrigor@plusquan.com>
2016-11-22 15:45:19 -0800

    rod

--- R/sd.R
+++ R/sd.R
@@ -1,3 +1,12 @@
+#' Standard Deviation
+#'
+#' @param x a vector of numbers
+#'
+#' @return the standard deviation
+#' @export
+#'
+#' @examples
+#' standstandard_deviation(c(1,4,7,2,8,4,9,1))
 standard_deviation <- function(x) {
   n <- length(x)
   mean = sum(x) / n
