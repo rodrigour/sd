77333cf5263e9ba4e2e3f2fa64e63cc7a0b3df04
Kitson Swann <kptswann@gmail.com>
2016-11-29 16:33:37 -0800

    Adding standard error function and updating documentation.

--- R/sd.R
+++ R/sd.R
@@ -6,7 +6,7 @@
 #' @export
 #'
 #' @examples
-#' standstandard_deviation(c(1,4,7,2,8,4,9,1))
+#' standard_deviation(c(1,4,7,2,8,4,9,1))
 standard_deviation <- function(x) {
   n <- length(x)
   mean = sum(x) / n
@@ -14,3 +14,14 @@ standard_deviation <- function(x) {
   stddev = sqrt(ssq/n)
   return(stddev)
 }
+
+#' Standard Error
+#'
+#' @param x a vector of numbers
+#'
+#' @return the standard error
+#' @export
+#'
+#' @examples
+#' standard_error(c(1,4,7,2,8,4,9,1))
+standard_error <- function(x) standard_deviation(x)/sqrt(length(x))
--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Standard Deviation Package
 
-This package has the following function:
-standard_deviation(x)
+This package has the following functions:
 
-Usage:
-
-standard_deviation(x)
-
-
-Inputs:
-
-x = values
+- standard_deviation(x)
+  - Usage: `standard_deviation(x)`
+  - Inputs: x, a vector of numeric values
+  - Outputs: numeric, standard deviation of x
+- standard_error(x)
+  - Usage: `standard_error(x)`
+  - Inputs: x, a vector of numeric values
+  - Outputs: numeric, standard error of x
